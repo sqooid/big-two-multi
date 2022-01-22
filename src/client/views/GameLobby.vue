bfb9240829d977e773443cfbf13d848eac2a5b09
sqooid <lucasliu7@hotmail.com>
2022-01-22 22:20:03 +1100

    Made cards

--- src/client/views/GameLobby.vue
+++ src/client/views/GameLobby.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="game-lobby">Hello</div>
+  <div class="game-lobby"></div>
 </template>
 
 <script lang="ts" setup>
 import router from '@/client/router'
+import PlayingCard from '@/client/components/PlayingCard.vue'
 
 // Lobby ID TODO
 // const id = router.currentRoute.value.params.id
