a06b0bb269c4ec3d24fce6aabe480ca0136dfb1b
sqooid <lucasliu7@hotmail.com>
2022-01-22 22:20:12 +1100

    Added cards

--- src/client/components/PlayingCard.vue
+++ src/client/components/PlayingCard.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>Hello</div>
+  <img :src="`/assets/cards/${suit}-${props.card.value}.svg`" />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onUpdated, ref } from 'vue'
+import CardSuit from '@/client/components/CardSuit.vue'
+import { Card, Suit } from '@sqooid/big-two'
 
-<style scoped></style>
+interface Props {
+  card: Card
+}
+const props = defineProps<Props>()
+
+let suit: string
+switch (props.card.suit) {
+  case Suit.DIAMOND:
+    suit = 'diamond'
+    break
+  case Suit.CLUB:
+    suit = 'club'
+    break
+  case Suit.HEART:
+    suit = 'heart'
+    break
+  default:
+    suit = 'spade'
+}
+</script>
+
+<style></style>
