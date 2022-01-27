115ab6c0809dbb904a47f7ba6f7197c8a78fe5f0
sqooid <lucasliu7@hotmail.com>
2022-01-27 15:25:41 +1100

    Moved client emits handlers into functions

--- src/server/event-handler.ts
+++ src/server/event-handler.ts
@@ -1,135 +1,44 @@
-import {
-  broadcastGame,
-  broadcastLobby,
-  broadcastUsersInLobby,
-  sendLobby,
-} from '@/server/send'
-import { createLobby, createUser, lobbyMap, userMap } from '@/server/maps'
-import { CallbackResults, ServerSocket } from '@/interfaces/socket-events'
-import { generateName } from '@/server/utils'
 import { Play } from '@sqooid/big-two'
 import {
-  cardToTupleString,
-  logGamePlays,
-  logGameStartingHands,
-  logToFile,
-} from '@/server/debug'
-import { ServerLobby } from '@/interfaces/server-interfaces'
-import { shuffleArray } from '@/interfaces/general-functions'
-import { handleChangeLobbySettings } from '@/server/handlers'
+  handleChangeLobbySettings,
+  handleChangeName,
+  handleCreateLobby,
+  handleCreateUser,
+  handleJoinLobby,
+  handleMakePlay,
+  handleStartGame,
+} from '@/server/handlers'
+import { ServerSocket } from '@/interfaces/socket-events'
 
 export function handleClientEmits(socket: ServerSocket) {
   // User creation
   socket.on('createUser', (name?: string) => {
-    name = name || generateName()
-    const newUser = createUser(socket.id, name)
-    socket.emit('syncUser', newUser)
+    handleCreateUser(socket, name)
   })
 
   // Lobby creation
   socket.on('createLobby', () => {
-    const host = userMap.get(socket.id)
-    if (!host) return
-    const newLobby = createLobby(host)
-    console.log('Created lobby:', newLobby.id)
-    host.lobby = newLobby
-    sendLobby(host)
+    handleCreateLobby(socket)
   })
 
   // Lobby join
   socket.on('joinLobby', (id: string, callback: (response: any) => void) => {
-    const user = userMap.get(socket.id)
-    const lobby = lobbyMap.get(id)
-    if (!user || !lobby) {
-      callback({ result: CallbackResults.FAILED })
-      return
-    }
-
-    const gameHasStarted = lobby.game.players.length > 0
-    const gameCanFitMorePlayers =
-      (!gameHasStarted && lobby.players.length >= 4) ||
-      (gameHasStarted && lobby.players.length >= lobby.game.players.length)
-    if (!gameCanFitMorePlayers) {
-      lobby.players.push(user)
-      lobby.settings.deal.playerCount = lobby.players.length
-    } else lobby.spectators.push(user)
-    user.lobby = lobby
-
-    console.log('Joined lobby:', lobby.id)
-
-    callback({ result: CallbackResults.SUCCESS })
-    broadcastLobby(lobby)
+    handleJoinLobby(socket, id, callback)
   })
 
   // Making plays
   socket.on('makePlay', (play?: Play) => {
-    const user = userMap.get(socket.id)
-    const game = user?.lobby?.game
-    if (!game) return
-
-    const validPlay = game.makePlay(play ?? undefined)
-    if (validPlay && user.lobby) {
-      broadcastGame(user.lobby)
-    }
-
-    // Logging
-    if (game.isFinished) {
-      logGamePlays(user.lobby as ServerLobby)
-    }
+    handleMakePlay(socket, play)
   })
 
   // Starting games
   socket.on('startGame', () => {
-    const user = userMap.get(socket.id)
-    const lobby = user?.lobby
-    if (!user || !lobby) return
-
-    const isHost = socket.id === lobby.host.socketId
-    if (!isHost) return
-
-    const validPlayerCount =
-      lobby.settings.deal.playerCount > 1 && lobby.settings.deal.playerCount < 5
-    if (!validPlayerCount) return
-
-    console.log('Started game: ', lobby.id)
-    const winnerIndex = lobby.game.winner
-    const isFirstGame = winnerIndex === undefined
-
-    lobby.game.dealCards(lobby.settings.deal)
-    const winner = lobby.players[winnerIndex ?? 0]
-    lobby.players = shuffleArray(lobby.players)
-    if (!isFirstGame && !lobby.settings.deal.randomHands) {
-      const winnerIndexInLobby = lobby.players.indexOf(winner)
-      if (winnerIndexInLobby !== -1) {
-        const tempPlayer = lobby.players[0]
-        lobby.players[0] = lobby.players[winnerIndexInLobby]
-        lobby.players[winnerIndexInLobby] = tempPlayer
-      }
-    }
-
-    broadcastGame(lobby)
-
-    // Logging
-    logGameStartingHands(lobby)
+    handleStartGame(socket)
   })
 
   // Changing IGN
   socket.on('changeName', (name) => {
-    const user = userMap.get(socket.id)
-    const lobby = user?.lobby
-    if (!user) return
-
-    let newName
-    if (!name) {
-      newName = generateName()
-    } else {
-      newName = name
-    }
-    user.name = newName
-    if (lobby) {
-      broadcastUsersInLobby(lobby)
-    }
-    socket.emit('syncUser', { name: newName })
+    handleChangeName(socket, name)
   })
 
   // Changing lobby settings
--- src/server/handlers.ts
+++ src/server/handlers.ts
@@ -1,7 +1,129 @@
 import { LobbySettings } from '@/interfaces/client-interfaces'
-import { ServerSocket } from '@/interfaces/socket-events'
-import { userMap } from '@/server/maps'
-import { broadcastLobbySettings } from '@/server/send'
+import { shuffleArray } from '@/interfaces/general-functions'
+import { ServerLobby } from '@/interfaces/server-interfaces'
+import { CallbackResults, ServerSocket } from '@/interfaces/socket-events'
+import { logGamePlays, logGameStartingHands } from '@/server/debug'
+import { createLobby, createUser, lobbyMap, userMap } from '@/server/maps'
+import {
+  broadcastGame,
+  broadcastLobby,
+  broadcastLobbySettings,
+  broadcastUsersInLobby,
+  sendLobby,
+} from '@/server/send'
+import { generateName } from '@/server/utils'
+import { Play } from '@sqooid/big-two'
+
+export function handleCreateUser(socket: ServerSocket, name?: string) {
+  name = name || generateName()
+  const newUser = createUser(socket.id, name)
+  socket.emit('syncUser', newUser)
+}
+
+export function handleCreateLobby(socket: ServerSocket) {
+  const host = userMap.get(socket.id)
+  if (!host) return
+  const newLobby = createLobby(host)
+  console.log('Created lobby:', newLobby.id)
+  host.lobby = newLobby
+  sendLobby(host)
+}
+
+export function handleJoinLobby(
+  socket: ServerSocket,
+  id: string,
+  callback: (response: any) => void,
+) {
+  const user = userMap.get(socket.id)
+  const lobby = lobbyMap.get(id)
+  if (!user || !lobby) {
+    callback({ result: CallbackResults.FAILED })
+    return
+  }
+
+  const gameHasStarted = lobby.game.players.length > 0
+  const gameCanFitMorePlayers =
+    (!gameHasStarted && lobby.players.length >= 4) ||
+    (gameHasStarted && lobby.players.length >= lobby.game.players.length)
+  if (!gameCanFitMorePlayers) {
+    lobby.players.push(user)
+    lobby.settings.deal.playerCount = lobby.players.length
+  } else lobby.spectators.push(user)
+  user.lobby = lobby
+
+  console.log('Joined lobby:', lobby.id)
+
+  callback({ result: CallbackResults.SUCCESS })
+  broadcastLobby(lobby)
+}
+
+export function handleMakePlay(socket: ServerSocket, play?: Play) {
+  const user = userMap.get(socket.id)
+  const game = user?.lobby?.game
+  if (!game) return
+
+  const validPlay = game.makePlay(play ?? undefined)
+  if (validPlay && user.lobby) {
+    broadcastGame(user.lobby)
+  }
+
+  // Logging
+  if (game.isFinished) {
+    logGamePlays(user.lobby as ServerLobby)
+  }
+}
+
+export function handleStartGame(socket: ServerSocket) {
+  const user = userMap.get(socket.id)
+  const lobby = user?.lobby
+  if (!user || !lobby) return
+
+  const isHost = socket.id === lobby.host.socketId
+  if (!isHost) return
+
+  const validPlayerCount =
+    lobby.settings.deal.playerCount > 1 && lobby.settings.deal.playerCount < 5
+  if (!validPlayerCount) return
+
+  console.log('Started game: ', lobby.id)
+  const winnerIndex = lobby.game.winner
+  const isFirstGame = winnerIndex === undefined
+
+  lobby.game.dealCards(lobby.settings.deal)
+  const winner = lobby.players[winnerIndex ?? 0]
+  lobby.players = shuffleArray(lobby.players)
+  if (!isFirstGame && !lobby.settings.deal.randomHands) {
+    const winnerIndexInLobby = lobby.players.indexOf(winner)
+    if (winnerIndexInLobby !== -1) {
+      const tempPlayer = lobby.players[0]
+      lobby.players[0] = lobby.players[winnerIndexInLobby]
+      lobby.players[winnerIndexInLobby] = tempPlayer
+    }
+  }
+
+  broadcastGame(lobby)
+
+  // Logging
+  logGameStartingHands(lobby)
+}
+
+export function handleChangeName(socket: ServerSocket, name: string) {
+  const user = userMap.get(socket.id)
+  const lobby = user?.lobby
+  if (!user) return
+
+  let newName
+  if (!name) {
+    newName = generateName()
+  } else {
+    newName = name
+  }
+  user.name = newName
+  if (lobby) {
+    broadcastUsersInLobby(lobby)
+  }
+  socket.emit('syncUser', { name: newName })
+}
 
 export function handleChangeLobbySettings(
   socket: ServerSocket,
